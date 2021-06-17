6a6bee9d03d1f426ea700070c57ef78b576100ca
Hernandes-Silva <lertanisslobr@gmail.com>
2021-06-17 15:04:11 -0300

    configurando o heroku

--- myapi/settings.py
+++ myapi/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
     'myapp',
     'rest_framework',
     'django_filters',
