8a85865d4a456253c66e29a0f113b2ca2a970651
Hernandes-Silva <lertanisslobr@gmail.com>
2021-06-17 11:40:21 -0300

    configurando o heroku

--- myapi/settings.py
+++ myapi/settings.py
@@ -121,7 +121,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sisraco.herokuapp.com']
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
